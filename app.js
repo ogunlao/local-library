0391a30cfbaec548864f9ae1b7d4ff506d3d9375
lao <ogunlao@yahoo.com>
2019-08-14 22:12:07 +0100

    Add author, book, bookinstance and genre models

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://127.0.0.1:27017/local_library';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
